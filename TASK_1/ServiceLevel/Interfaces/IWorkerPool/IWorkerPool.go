8b2271bba72835b845bc17b94a40693f190622cc
glkru <gl.krutoi@mail.ru>
2025-09-29 11:44:36 +0300

    Update (ServiceLevel/Interfaces/IWorkerPool/IWorkerPool.go) :  сделал интерфейсы
    1)Queue - отвечает за очередь
    2)StateStore  - получатор и установлятор значений сотостояний
    3) JobProcessor  будем прокидывать процессу

--- TASK_1/ServiceLevel/Interfaces/IWorkerPool/IWorkerPool.go
+++ TASK_1/ServiceLevel/Interfaces/IWorkerPool/IWorkerPool.go
@@ -1,1 +1,21 @@
 package IWorkerPool
+
+import (
+	"Kaspersky_Go/ModeLevel/Structures"
+	"context"
+)
+
+type Queue interface {
+	Enqueue(job Structures.Job) error
+	Dequeue() (Structures.Job, bool)
+	Close()
+}
+
+type StateStore interface {
+	Set(id string, st Structures.JobStatus)
+	Get(id string) (Structures.JobStatus, bool)
+}
+
+type JobProcessor interface {
+	Process(ctx context.Context, job Structures.Job) Structures.JobStatus
+}
--- TASK_1/ServiceLevel/UseCases/WokerPool/WokerPool.go
+++ TASK_1/ServiceLevel/UseCases/WokerPool/WokerPool.go
@@ -1,7 +1,51 @@
 package WokerPool
 
-import "Kaspersky_Go/ModeLevel/Structures"
+import (
+	"Kaspersky_Go/ModeLevel/Structures"
+	"Kaspersky_Go/ServiceLevel/Interfaces/IWorkerPool"
+	"context"
+)
 
-type Queue struct {
-	ch chan Structures.JobS
+type WorkerPool struct {
+	workers   int
+	queue     IWorkerPool.Queue
+	state     IWorkerPool.StateStore
+	processor IWorkerPool.JobProcessor
+}
+
+func NewWorkerPool(workers int, queue IWorkerPool.Queue, state IWorkerPool.StateStore,
+	processor IWorkerPool.JobProcessor) *WorkerPool {
+
+	return &WorkerPool{
+		workers:   workers,
+		queue:     queue,
+		state:     state,
+		processor: processor,
+	}
+}
+
+func (wp *WorkerPool) Start(ctx context.Context) {
+	for i := 0; i < wp.workers; i++ {
+		go func(workerId int) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					job, ok := wp.queue.Dequeue()
+					if !ok {
+						return
+					}
+
+					status := wp.processor.Process(ctx, job)
+					wp.state.Set(job.ID, status)
+
+					if status.State == Structures.StateQueued {
+						_ = wp.queue.Enqueue(job)
+					}
+				}
+
+			}
+		}(i)
+	}
 }
