59737e59c2db47b3faf5cf35f70604bbdaa73a21
glkru <gl.krutoi@mail.ru>
2025-09-29 10:56:27 +0300

    FEAT (TASK1/modelLevel) : создал файл со структурами

--- TASK_1/ModeLevel/Structures/Structs.go
+++ TASK_1/ModeLevel/Structures/Structs.go
@@ -1,1 +1,29 @@
 package Structures
+
+type JobState string
+
+const (
+	StateQueued  JobState = "queued"
+	StateRunning JobState = "running"
+	StateDone    JobState = "done"
+	StateFailed  JobState = "failed"
+)
+
+type EnqueueRequest struct {
+	ID         string `json:"id"`
+	Payload    string `json:"payload"`
+	MaxRetries int    `json:"max_retries"`
+}
+
+type Job struct {
+	ID         string
+	Payload    string
+	MaxRetries int
+	Attempts   int
+}
+
+type JobStatus struct {
+	State    JobState
+	Attempts int
+	Error    string
+}
