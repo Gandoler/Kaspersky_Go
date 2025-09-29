d1c819cb4e086d1a4533c1f7e206b313d08a7394
glkru <gl.krutoi@mail.ru>
2025-09-29 12:11:18 +0300

    Update (TASK_1/ServiceLevel/UseCases/Processors/RetryProcessor.go) :  сделал реализацию процессора, но споткнулся на моменте статуса, нужно поменять его на передачу внутрь этой функции, так как статус нужно менять и во время выполнения

--- TASK_1/ServiceLevel/UseCases/Processors/RetryProcessor.go
+++ TASK_1/ServiceLevel/UseCases/Processors/RetryProcessor.go
@@ -1,1 +1,39 @@
 package Processors
+
+import (
+	"Kaspersky_Go/ModeLevel/Structures"
+	"context"
+	"math/rand"
+	"time"
+)
+
+type RetryProcessor struct{}
+
+func (p *RetryProcessor) Process(ctx context.Context, job Structures.Job, &) Structures.JobStatus {
+
+	status := Structures.JobStatus{State: Structures.StateRunning, Attempts: job.Attempts}
+
+	time.Sleep(time.Duration(100+rand.Intn(50)) * time.Millisecond)
+
+	if rand.Intn(100) < 20 {
+		job.Attempts++
+		if job.Attempts <= job.MaxRetries {
+
+			delay := backoff(job.Attempts)
+			time.Sleep(delay)
+			return Structures.JobStatus{
+				State:    Structures.StateQueued,
+				Attempts: job.Attempts,
+				Error:    "retrying",
+			}
+		}
+	}
+
+	return Structures.JobStatus{State: Structures.StateDone, Attempts: job.Attempts}
+}
+
+func backoff(attempt int) time.Duration {
+	base := time.Duration(100*(1<<uint(attempt-1))) * time.Millisecond
+	jitter := time.Duration(rand.Intn(101)) * time.Millisecond
+	return base + jitter
+}
