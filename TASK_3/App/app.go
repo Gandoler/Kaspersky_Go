5973a308930b0e12b3bef017127f5885bcbbd217
glkru <gl.krutoi@mail.ru>
2025-09-30 10:53:55 +0300

    FEAT (TASK3) : начал реализацию  workerpool

--- TASK_3/App/app.go
+++ TASK_3/App/app.go
@@ -1,1 +1,41 @@
 package App
+
+import (
+	"errors"
+	"log/slog"
+)
+
+type Pool interface {
+	Submit(task func()) error
+	Stop() error
+}
+
+var (
+	ErrStopped   = errors.New("pool stopped")
+	ErrQueueFull = errors.New("queue full")
+)
+
+type Config struct {
+	Workers   int
+	QueueSize int
+	afterHook func()
+}
+
+func NewPool(config Config, log *slog.Logger) (Pool, error) {
+	if config.Workers < 1 {
+		return nil, errors.New("workers must be greater than zero")
+	}
+	if config.QueueSize < 1 {
+		return nil, errors.New("queue_size must be greater than zero")
+	}
+
+	p := &WorkerPool.workerPool{
+		queue:     make(chan func(), config.QueueSize),
+		afterHook: config.afterHook,
+	}
+
+	p.wgWorkers.Add(config.Workers)
+	for i := 0; i < config.Workers; i++ {
+		go p.worker()
+	}
+}
--- TASK_3/WorkerPool/WorkerPool.go
+++ TASK_3/WorkerPool/WorkerPool.go
@@ -1,1 +1,17 @@
 package WorkerPool
+
+import (
+	"log/slog"
+	"sync"
+)
+
+type workerPool struct {
+	queue     chan func()
+	stopOnce  sync.Once
+	stopped   bool
+	stateMu   sync.Mutex
+	wgTasks   sync.WaitGroup
+	wgWorkers sync.WaitGroup
+	afterHook func()
+	logger    *slog.Logger
+}
