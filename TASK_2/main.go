34f84667caad1d8d910957a1b67984ff78380c83
glkru <gl.krutoi@mail.ru>
2025-10-01 10:25:59 +0300

    UPDATE (TASK_2/main.go) : реализовал сценарий поведения в мэине

--- TASK_2/main.go
+++ TASK_2/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"TASK_2/WorkerPool/Realization"
 	"fmt"
+	"time"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
+	wp := Realization.NewWorkerPool(3)
 
+	for i := 1; i <= 5; i++ {
+		num := i // for thread safety
+		wp.Submit(func() {
+			fmt.Printf("Start task %d\n", num)
+			time.Sleep(1 * time.Second)
+			fmt.Printf("Done task %d\n", num)
+		})
+	}
+	fmt.Println("All tasks submitted\n\n")
+
+	wp.SubmitWait(func() {
+		fmt.Println("SubmitWait task started")
+		time.Sleep(2 * time.Second)
+		fmt.Println("SubmitWait task finished")
+	})
+
+	fmt.Println("All tasks submitted\n\n")
+
+	wp.StopWait()
+	fmt.Println("WorkerPool stopped")
 }
