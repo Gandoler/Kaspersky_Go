664c5280ad34ae2db6a9917db8b8e311568053cb
glkru <gl.krutoi@mail.ru>
2025-10-01 09:50:05 +0300

    FEAT (TASK_2/WorkerPool/) : создал файл для интерфейса пула и его реализации

---
+++ TASK_2/WorkerPool/Realization/WorkerPool_base.go
@@ -0,0 +1,24 @@
+package WorkerPool
+
+type WorkerPool struct {
+}
+
+func NewWorkerPool(numberOfWorkers int) *WorkerPool {
+	return &WorkerPool{}
+}
+
+func (wp *WorkerPool) Submit(task func()) {
+
+}
+
+func (wp *WorkerPool) SubmitWait(task func()) {
+
+}
+
+func (wp *WorkerPool) Stop() {
+
+}
+
+func (wp *WorkerPool) StopWait() {
+
+}
