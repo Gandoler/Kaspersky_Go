9310d98f877abf907e482b263686bc30eaff73c6
glkru <gl.krutoi@mail.ru>
2025-10-01 10:16:29 +0300

    FEAT (TASK_2/WorkerPool/Realization/WorkerPool_base.go) : сделал реализацию пула на основе пула из 3 задания

--- TASK_2/WorkerPool/Realization/WorkerPool_base.go
+++ TASK_2/WorkerPool/Realization/WorkerPool_base.go
@@ -1,24 +1,108 @@
 package Realization
 
+import (
+	"sync"
+)
+
 type WorkerPool struct {
+	queue     chan func()
+	stopOnce  sync.Once
+	stopped   bool
+	stateMu   sync.Mutex
+	wgTasks   sync.WaitGroup
+	wgWorkers sync.WaitGroup
 }
 
 func NewWorkerPool(numberOfWorkers int) *WorkerPool {
-	return &WorkerPool{}
+	if numberOfWorkers < 1 {
+		return nil
+	}
+
+	pool := &WorkerPool{
+		queue: make(chan func(), 64), // default value
+	}
+
+	pool.wgWorkers.Add(numberOfWorkers)
+	for i := 0; i < numberOfWorkers; i++ {
+		go func(id int) {
+			defer pool.wgWorkers.Done()
+			for task := range pool.queue {
+				task()
+				pool.wgTasks.Done()
+			}
+		}(i)
+	}
+	return pool
 }
 
 func (wp *WorkerPool) Submit(task func()) {
+	if task == nil {
+		return
+	}
+
+	wp.stateMu.Lock()
+	if wp.stopped {
+		wp.stateMu.Unlock()
+		return
+	}
+
+	if len(wp.queue) == cap(wp.queue) {
+		wp.stateMu.Unlock()
+		return
+	}
+	wp.wgTasks.Add(1)
+	wp.stateMu.Unlock()
+
+	wp.queue <- task
+	return
 
 }
 
 func (wp *WorkerPool) SubmitWait(task func()) {
+	if task == nil {
+		return
+	}
 
+	wp.stateMu.Lock()
+	if wp.stopped {
+		wp.stateMu.Unlock()
+		return
+	}
+
+	if len(wp.queue) == cap(wp.queue) {
+		wp.stateMu.Unlock()
+		return
+	}
+	wp.wgTasks.Add(1)
+	wp.stateMu.Unlock()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	wp.queue <- func() {
+		defer wg.Done()
+		task()
+	}
+
+	wg.Wait()
+	return
 }
 
 func (wp *WorkerPool) Stop() {
+	wp.stopOnce.Do(func() {
+		wp.stateMu.Lock()
+		wp.stopped = true
+		wp.stateMu.Unlock()
 
+		close(wp.queue)
+		wp.wgWorkers.Wait()
+	})
+
+	return
 }
 
 func (wp *WorkerPool) StopWait() {
-
+	wp.Stop()
+	wp.wgTasks.Wait()
+	wp.wgWorkers.Wait()
 }
